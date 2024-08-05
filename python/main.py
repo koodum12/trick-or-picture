65ae2c6737a695ee50ee0db7fc7ea49399ae474a
minchaehong <minchaehong2@gmail.com>
2024-08-05 09:07:57 +0900

    first commit

--- python/main.py
+++ python/main.py
@@ -12,27 +12,30 @@ with mp_face_mesh.FaceMesh(
         max_num_faces=1,
         refine_landmarks=True,
         min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as face_mesh:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("웹캠을 찾을 수 없습니다.")
-
+        min_tracking_confidence=0.5) as face_mesh: #1.얼굴 감지 갯수,2.랜드마크 정밀 조작,3.얼굴 감지 정확도 최소,4.얼굴 추적 최소 정확도.
+    while cap.isOpened():#웹캠 열려있을 때.
+        success, image = cap.read()#프레임 읽어오는 함수 success = 제대로 읽혔는지 image = 프레임별 이미지
+        if not success:#
+            print("웹캠을 찾을 수 없습니다,큰일났습니다.")
             break
 
-        image.flags.writeable = True
-        results = face_mesh.process(image)
+        image.flags.writeable = False
+        results = face_mesh.process(image)#얼굴 렌드마크 처리
 
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)#rgb값으로 변경
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
-                save_image = picture.start_take(mp_drawing,image,face_landmarks,mp_face_mesh)
-                
-            keyboard.add_hotkey('p',picture.picture_save(save_image))#사용자 지정 함수 넣을 예정
-                            
+                save_image = picture.start_take(mp_drawing,image,face_landmarks,mp_face_mesh)#프레임 단위로 실시간 이미지 변수 처리.
+            
+            if cv2.waitKey(1) & 0xFF ==ord('p'):
+                picture.picture_save(save_image)
         image_bgr = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
-        cv2.imshow('MediaPipe Face Mesh(Puleugo)', cv2.flip(image_bgr, 1))
-        if cv2.waitKey(5) & 0xFF == 27:
+        cv2.imshow('MediaPipe Face Mesh(Puleugo)', cv2.flip(image_bgr, 1))#
+        if cv2.waitKey(1) & 0xFF == ord('q'):#q를 누르면 while 문 나가기 
             break
-cap.release()
+        '''
+            1.영상 촬영 정지
+            2.landmark표시 정지
+            3.그 안에 있는 모든 것.
+        '''
+cap.release()#비디오 캡쳐 창 끄기(if 문 안에 break대신 넣을 예정)
--- python/picture.py
+++ python/picture.py
@@ -4,15 +4,19 @@ import keyboard
 
 def __init__(self):
     pass
+
 def start_take(mp_drawing,image,face_landmarks,mp_face_mesh):
-  mp_drawing_styles =mp_drawing.DrawingSpec(color=(0,0,0),thickness = 10 ) #
+  
+  mp_drawing_styles =mp_drawing.DrawingSpec(color=(0,0,0),thickness = 10 ) #랜드마크 위에 object를 어떻게 띄울지 (mediapipe 함수)
   save_image = mp_drawing.draw_landmarks(
     image=image,
     landmark_list=face_landmarks,
     connections=mp_face_mesh.FACEMESH_IRISES,
     landmark_drawing_spec=None,connection_drawing_spec=mp_drawing_styles)
+  
   return save_image
     
 def picture_save(save_image):
-  print("아직 구현 안됨")
-  return
+  #cv2.imshow('o',save_image)
+  print(1)
+  return 
